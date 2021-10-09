028b337957c39cc5416c883c3350b429159661eb
unknown <bensonea@appstate.edu>
2021-10-08 20:04:41 -0400

    Improved comments

--- asyncBuffer.py
+++ asyncBuffer.py
@@ -1,5 +1,4 @@
-#import pandas as pd
-import os
+from os import mkdir
 from os.path import exists
 from pandas import DataFrame, Series
 
@@ -14,16 +13,25 @@ class Buffer:
         self.working_candle = dict()
         self.init_directories()
 
+    #
+    # Print out information about all of the datasets
+    #
     def __repr__(self) -> str:
         s = "Buffer => "
         for dataset in self.datasets:
+            # Print the interval and the size of its dataset
             s += f"{dataset} - (size={len(self.datasets[dataset])})\t"
         return s
 
+    #
+    # Check to ensure the required directories exist
+    #
     def init_directories(self) -> None:
         if not exists("csvs/"):
-            os.mkdir("csvs")
-
+            mkdir("csvs")
+    #
+    # Adds a candle from the websocket listener to the correct dataset
+    #
     def recv_candle(self, candle: dict) -> None:
         interval = candle["i"]
         print(f"Received {interval} candle")
@@ -32,6 +40,7 @@ class Buffer:
             try:
                 df = self.datasets[interval]
             except KeyError:
+                # If there is no data for this candle interval, make a new dataframe for it
                 cols = list(candle.keys())
                 self.datasets[interval] = DataFrame(columns=cols)
             finally:
@@ -42,24 +51,33 @@ class Buffer:
                 if self.save_data:
                     csv_path = self.csv_format.format(candle["s"], interval)
                     self.datasets[interval].append(self.working_candle[interval]).to_csv(csv_path)
+        # If the candle that was just received isn't a closing candle, update the working_candle
         else:
             candle_series = Series(candle)
             self.working_candle[interval] = candle_series
 
+    #
+    # Returns the dataset for a given candle stick interval of a specified size.
+    # If there is no size given as a parameter, it will use the default size in __init__
+    #
     def get_dataset(self, interval: str, size=None) -> DataFrame:
         size = size if size else self.size
         try:
-
+            # Check if there is current data for this interval
             df = self.datasets[interval][-size:]
             try:
+                # Append the working candle to the current data
                 wc = self.working_candle[interval]
                 return df.append(wc, ignore_index=True)
             except KeyError:
+                # There is no working_candle for this interval
                 return df[-size:]
         except KeyError:
             try:
+                # There is no current data for this interval
                 return DataFrame(self.working_candle[interval], index=self.working_candle[interval].index)
             except KeyError:
+                # There is no current data or working candle for this interval
                 return DataFrame()
 
 
--- asyncStream.py
+++ asyncStream.py
@@ -17,13 +17,25 @@ data_buffer = Buffer(save_data=True)
 
 def main():
     symbol = "ETHUSDT"
-    sock = ThreadedWebsocketManager(api_key, api_secret, tld="us")
+    sock = ThreadedWebsocketManager(api_key,
+                                    api_secret,
+                                    tld="us")
     sock.start()
 
+    # This is called every time we receive candle data from the Binance server
     def on_message(msg):
+        # Add the candle we just received to the data buffer
         data_buffer.recv_candle(msg["k"])
         print(data_buffer)
         #print(data_buffer.get_dataset(msg["k"]["i"]))
+
+        # TODO: Pass the data buffer to the strategy to update
+        #   Ask the strategy for an action
+        #  Also make a new strategy that considers more than one candle interval with states
+
+        # TODO: An action should be on of:
+        #   Wait to buy, hold position, buy position, sell position
+        #  Based on the action, do something in the interface
 
     sock.start_kline_socket(callback=on_message, symbol=symbol, interval=INTERVAL_1M)
     sock.start_kline_socket(callback=on_message, symbol=symbol, interval=INTERVAL_5M)
