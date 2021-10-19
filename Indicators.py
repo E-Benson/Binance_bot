Fixed supertrend calculation and cleaned up ADX functions

Indicators.py
@@ -85,34 +85,11 @@ class ADX:
 
         self.data["adx"] = s_df["adx"]
 
-    def _adx_pos(self):
+    def _dx(self, input_col):
         size = len(self.data)
         # Make an array of pairs containing the high price and 0
         dm = np.zeros((size, 2))
-        dm[:, 1] = self.data["h"].to_numpy().reshape((size, ))
-        # Find if the current day's high was higher than yesterday's, otherwise 0
-        dm = np.max(np.diff(self.rolling(dm, 2), axis=0), axis=2)
-        # Fill in first missing item from np.diff
-        dm = np.append(np.zeros((1,)), dm)
-
-        # Sum that past period's dm
-        dma = DataFrame(dm)
-        fill = np.zeros((self.period, ))
-        dma.at[self.period-1] = dma[:self.period].sum()
-        dma = dma[self.period-1:].rolling(2).apply(self.smooth)
-
-        dma["atr"] = self.data["atr"]
-        divs = dma[dma["atr"] > 0].apply(lambda a: np.divide(a, dma.loc[a.index]["atr"]))[0]
-        dma[0][dma["atr"] > 0] = divs
-        dma_array = np.append(fill, dma[0][1:])
-
-        self.data["pdma"] = dma_array * 100
-
-    def _adx_neg(self):
-        size = len(self.data)
-        # Make an array of pairs containing the high price and 0
-        dm = np.zeros((size, 2))
-        dm[:, 1] = self.data["l"].to_numpy().reshape((size,))
+        dm[:, 1] = self.data[input_col].to_numpy().reshape((size,))
         # Find if the current day's high was higher than yesterday's, otherwise 0
         dm = np.max(np.diff(self.rolling(dm, 2), axis=0), axis=2)
         # Fill in first missing item from np.diff
@@ -129,7 +106,13 @@ class ADX:
         dma[0][dma["atr"] > 0] = divs
         dma_array = np.append(fill, dma[0][1:])
 
-        self.data["ndma"] = dma_array * 100
+        return dma_array * 100
+
+    def _adx_pos(self):
+        self.data["pdma"] = self._dx("h")
+
+    def _adx_neg(self):
+        self.data["ndma"] = self._dx("l")
 
 
 class SuperTrend:
@@ -149,9 +132,7 @@ class SuperTrend:
 
     def _atr(self):
         def true_range(a): return max([a["h"] - a["l"], abs(a["h"] - a["c"]), abs(a["l"] - a["c"])])
-        #self.data["tr"] = self.data.apply(true_range, axis=1)
         self.data = self.data.assign(tr=self.data.apply(true_range, axis=1))
-        #self.data[:, "atr"] = self.data["tr"].rolling(self.period).apply(lambda a: (1 / self.period) * a.sum())
         self.data = self.data.assign(atr=self.data["tr"]
                                              .rolling(self.period)
                                              .apply(lambda a: (1 / self.period) * a.sum()))
@@ -164,34 +145,46 @@ class SuperTrend:
         self.data["fuband"] = self.data["uband"]
         self.data["flband"] = self.data["lband"]
         self.data["trend"] = True
-        #print(self.data[["c", "atr", "uband", "lband", "fuband", "trend"]])
+        self.data["supertrend"] = np.nan
         for i, frame in self.data[1:].iterrows():
             p = i - 1
             c = i
-            # current close > previous upper band
-            if self.data.loc[c]["c"] > self.data.loc[p]["uband"]:
+            # current close > previous final upper band
+            if self.data.loc[c]["c"] > self.data.loc[p]["fuband"]:
                 self.data.at[c, "trend"] = True
-            # current close < previous lower band
-            elif self.data.loc[c]["c"] < self.data.loc[p]["lband"]:
+            # current close < previous final lower band
+            elif self.data.loc[c]["c"] < self.data.loc[p]["flband"]:
                 self.data.at[c, "trend"] = False
-            # previous upper band > current close > previous lower band
+            # previous final upper band > current close > previous final lower band
             else:
                 self.data.at[c, "trend"] = self.data.loc[p]["trend"]
-                # current price above previous lower band AND current lower band < previous lower band
-                if self.data.loc[c]["trend"] and self.data.loc[c]["lband"] < self.data.loc[p]["lband"]:
-                    self.data.at[c, "flband"] = self.data.loc[p]["lband"]
-                # current price below previous lower band AND current upper band > previous upper
-                if not self.data.loc[c]["trend"] and self.data.loc[c]["uband"] > self.data.loc[p]["uband"]:
-                    self.data.at[c, "fuband"] = self.data.loc[p]["uband"]
 
-            if self.data.loc[c]["c"] <= self.data.loc[p]["flband"]:
+                # trending AND current final lower band < previous final lower band
+                if self.data.loc[c]["trend"] and self.data.loc[c]["flband"] < self.data.loc[p]["flband"]:
+                    self.data.at[c, "flband"] = self.data.loc[p]["flband"]
+                # not trending AND current final upper band > previous final upper band
+                if not self.data.loc[c]["trend"] and self.data.loc[c]["fuband"] > self.data.loc[p]["fuband"]:
+                    self.data.at[c, "fuband"] = self.data.loc[p]["fuband"]
+
+            # current price below previous supertrend line
+            if self.data.loc[c]["c"] <= self.data.loc[p]["supertrend"]:
                 self.data.at[c, "supertrend"] = self.data.loc[c]["fuband"]
             else:
                 self.data.at[c, "supertrend"] = self.data.loc[c]["flband"]
 
+    def entries(self):
+        d = DataFrame(columns=["Date", "Entry"])
+        d["Date"] = self.data["t"]
+        d["Entry"] = self.data["c"] > self.data["supertrend"]
+        d = d.set_index(d["Date"]).drop("Date", axis=1)
+        return d["Entry"]
 
-
-
+    def exits(self):
+        d = DataFrame(columns=["Date", "Exit"])
+        d["Date"] = self.data["t"]
+        d["Exit"] = self.data["c"] < self.data["supertrend"]
+        d = d.set_index(d["Date"]).drop("Date", axis=1)
+        return d["Exit"]
 
     def bands(self):
         return self.data[["t", "uband", "lband"]]
@@ -199,6 +192,8 @@ class SuperTrend:
 
 if __name__ == "__main__":
     df = read_csv("csvs/ETHUSDT_3day_history.csv").drop("Unnamed: 0", axis=1)[:500]
-    st = SuperTrend(init_data=df[:50])
-    print(st.bands())
+    st = SuperTrend(init_data=df[:150])
+    #print(st.bands())
+    print(st.entries())
+    print(st.exits())
 